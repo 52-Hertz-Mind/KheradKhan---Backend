8cd32aad25b792841ff0c162b108601b64339ccd
Ali Zoghi <65258707+AliZoghi@users.noreply.github.com>
2024-12-07 19:28:30 +0330

    [Master-INIT] Init liar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
@@ -13,8 +15,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-
-  await app.listen(3000);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
